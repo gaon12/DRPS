818e6dab8c3d5d073536c0080fd759bcf83a589d
Sajandora <rasca99@naver.com>
2024-12-03 15:11:27 +0900

    icon->img 및 스타일코드 분리

--- frontend/CodeClear/tabs/SafetyGuide/pages/NaturalDisasters.js
+++ frontend/CodeClear/tabs/SafetyGuide/pages/NaturalDisasters.js
@@ -1,6 +1,32 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, FlatList, Modal, ScrollView, TouchableWithoutFeedback, StyleSheet } from 'react-native';
-import { MaterialIcons, Ionicons, FontAwesome, MaterialCommunityIcons, FontAwesome5 } from '@expo/vector-icons';
+import { Image, View, Text, TextInput, TouchableOpacity, FlatList, Modal, ScrollView, TouchableWithoutFeedback, StyleSheet } from 'react-native';
+import { MaterialIcons, Ionicons } from '@expo/vector-icons';
+import styles from './DisasterGuideStyle';
+
+const images = {
+  '01001.png': require('./icon_pic/NaturalDisasters/01001.png'),
+  '01002.png': require('./icon_pic/NaturalDisasters/01002.png'),
+  '01003.png': require('./icon_pic/NaturalDisasters/01003.png'),
+  '01004.png': require('./icon_pic/NaturalDisasters/01004.png'),
+  '01005.png': require('./icon_pic/NaturalDisasters/01005.png'),
+  '01006.png': require('./icon_pic/NaturalDisasters/01006.png'),
+  '01007.png': require('./icon_pic/NaturalDisasters/01007.png'),
+  '01008.png': require('./icon_pic/NaturalDisasters/01008.png'),
+  '01009.png': require('./icon_pic/NaturalDisasters/01009.png'),
+  '01010.png': require('./icon_pic/NaturalDisasters/01010.png'),
+  '01011.png': require('./icon_pic/NaturalDisasters/01011.png'),
+  '01012.png': require('./icon_pic/NaturalDisasters/01012.png'),
+  '01013.png': require('./icon_pic/NaturalDisasters/01013.png'),
+  '01014.png': require('./icon_pic/NaturalDisasters/01014.png'),
+  '01015.png': require('./icon_pic/NaturalDisasters/01015.png'),
+  '01016.png': require('./icon_pic/NaturalDisasters/01016.png'),
+  '01017.png': require('./icon_pic/NaturalDisasters/01017.png'),
+  '01018.png': require('./icon_pic/NaturalDisasters/01018.png'),
+  '01019.png': require('./icon_pic/NaturalDisasters/01019.png'),
+  '01020.png': require('./icon_pic/NaturalDisasters/01020.png'),
+  '01021.png': require('./icon_pic/NaturalDisasters/01021.png'),
+  '01022.png': require('./icon_pic/NaturalDisasters/01022.png'),
+};
 
 const MainScreen = ({ navigation }) => {
   const [showList, setShowList] = useState(false); // 전체보기 리스트 표시 여부
@@ -16,47 +42,40 @@ const MainScreen = ({ navigation }) => {
 
   // 재난 정보와 매핑되는 ID 리스트
   const modalDataList = [
-    { id: '1', mappingId: '01001', title: '가뭄', icon: 'sunny', iconType: 'Ionicons' },
-    { id: '2', mappingId: '01002', title: '강풍', icon: 'weather-windy', iconType: 'MaterialCommunityIcons' },
-    { id: '3', mappingId: '01003', title: '낙뢰', icon: 'thunderstorm-outline', iconType: 'Ionicons' },
-    { id: '4', mappingId: '01004', title: '대설', icon: 'weather-snowy-heavy', iconType: 'MaterialCommunityIcons' },
-    { id: '5', mappingId: '01005', title: '산사태', icon: 'terrain', iconType: 'MaterialCommunityIcons' },
-    { id: '6', mappingId: '01006', title: '우주전파재난', icon: 'radio', iconType: 'Ionicons' },
-    { id: '7', mappingId: '01007', title: '자연우주물체추락', icon: 'planet', iconType: 'Ionicons' },
-    { id: '8', mappingId: '01008', title: '적조', icon: 'leaf', iconType: 'Ionicons' },
-    { id: '9', mappingId: '01009', title: '조류대발생(녹조)', icon: 'leaf', iconType: 'Ionicons' },
-    { id: '10', mappingId: '01010', title: '조수', icon: 'water', iconType: 'Ionicons' },
-    { id: '11', mappingId: '01011', title: '지진', icon: 'water', iconType: 'Ionicons' },
-    { id: '12', mappingId: '01012', title: '지진해일', icon: 'water', iconType: 'Ionicons' },
-    { id: '13', mappingId: '01013', title: '침수', icon: 'water', iconType: 'Ionicons' },
-    { id: '14', mappingId: '01014', title: '태풍', icon: 'weather-hurricane', iconType: 'MaterialCommunityIcons' },
-    { id: '15', mappingId: '01015', title: '폭염', icon: 'sunny-outline', iconType: 'Ionicons' },
-    { id: '16', mappingId: '01016', title: '풍랑', icon: 'waves', iconType: 'MaterialCommunityIcons' },
-    { id: '17', mappingId: '01017', title: '한파', icon: 'snow', iconType: 'Ionicons' },
-    { id: '18', mappingId: '01018', title: '해일', icon: 'water', iconType: 'Ionicons' },
-    { id: '19', mappingId: '01019', title: '호우', icon: 'rainy-outline', iconType: 'Ionicons' },
-    { id: '20', mappingId: '01020', title: '홍수', icon: 'water', iconType: 'Ionicons' },
-    { id: '21', mappingId: '01021', title: '화산폭발', icon: 'water', iconType: 'Ionicons' },
-    { id: '22', mappingId: '01022', title: '황사', icon: 'water', iconType: 'Ionicons' },
-];
+    { id: '1', mappingId: '01001', title: '가뭄', imgName: '01001.png' },
+    { id: '2', mappingId: '01002', title: '강풍', imgName: '01002.png' },
+    { id: '3', mappingId: '01003', title: '낙뢰', imgName: '01003.png' },
+    { id: '4', mappingId: '01004', title: '대설', imgName: '01004.png' },
+    { id: '5', mappingId: '01005', title: '산사태', imgName: '01005.png' },
+    { id: '6', mappingId: '01006', title: '우주전파재난', imgName: '01006.png' },
+    { id: '7', mappingId: '01007', title: '자연우주물체추락', imgName: '01007.png' },
+    { id: '8', mappingId: '01008', title: '적조', imgName: '01008.png' },
+    { id: '9', mappingId: '01009', title: '조류대발생(녹조)', imgName: '01009.png' },
+    { id: '10', mappingId: '01010', title: '조수', imgName: '01010.png' },
+    { id: '11', mappingId: '01011', title: '지진', imgName: '01011.png' },
+    { id: '12', mappingId: '01012', title: '지진해일', imgName: '01012.png' },
+    { id: '13', mappingId: '01013', title: '침수', imgName: '01013.png' },
+    { id: '14', mappingId: '01014', title: '태풍', imgName: '01014.png' },
+    { id: '15', mappingId: '01015', title: '폭염', imgName: '01015.png' },
+    { id: '16', mappingId: '01016', title: '풍랑', imgName: '01016.png' },
+    { id: '17', mappingId: '01017', title: '한파', imgName: '01017.png' },
+    { id: '18', mappingId: '01018', title: '해일', imgName: '01018.png' },
+    { id: '19', mappingId: '01019', title: '호우', imgName: '01019.png' },
+    { id: '20', mappingId: '01020', title: '홍수', imgName: '01020.png' },
+    { id: '21', mappingId: '01021', title: '화산폭발', imgName: '01021.png' },
+    { id: '22', mappingId: '01022', title: '황사', imgName: '01022.png' },
+  ];
 
-  const allDisastersList = modalDataList.map(({ id, title }) => ({ id, title }));
-
-  // 아이콘 컴포넌트를 동적으로 가져오는 함수
-  const getIconComponent = (iconType, iconName, size = 35, color = 'black', style = {}) => {
-    if (iconType === 'Ionicons') {
-      return <Ionicons name={iconName} size={size} color={color} style={style} />;
-    } else if (iconType === 'FontAwesome') {
-      return <FontAwesome name={iconName} size={size} color={color} style={style} />;
-    } else if (iconType === 'MaterialCommunityIcons') {
-      return <MaterialCommunityIcons name={iconName} size={size} color={color} style={style} />;
-    } else if (iconType === 'FontAwesome5') {
-      return <FontAwesome5 name={iconName} size={size} color={color} style={style} />;
+  const getImageSource = (imgName) => {
+    if (images[imgName]) {
+      return images[imgName];
     } else {
-      console.warn(`Unknown icon type: ${iconType}`);
+      console.warn(`Image not found for: ${imgName}`);
       return null;
     }
   };
+
+  const allDisastersList = modalDataList.map(({ id, title }) => ({ id, title }));
 
   // 재난의 선택 상태를 관리
   const handleDisasterSelection = (title) => {
@@ -87,7 +106,6 @@ const MainScreen = ({ navigation }) => {
     }
   };
 
-
   const getButtonStyle = (title) => {
     return activeDisasters.includes(title) ? { backgroundColor: '#B0E0E6' } : { backgroundColor: '#f0f0f0' };
   };
@@ -114,11 +132,12 @@ const MainScreen = ({ navigation }) => {
           style={[styles.bigButton, getButtonStyle(selectedDisasters.bigButton1)]}
           onPress={() => handleItemPress(selectedDisasters.bigButton1)}
         >
-          {getIconComponent(
-            modalDataList.find(item => item.title === selectedDisasters.bigButton1)?.iconType || 'MaterialCommunityIcons',
-            modalDataList.find(item => item.title === selectedDisasters.bigButton1)?.icon || 'cloud',
-            65, 'black', styles.buttonIcon
-          )}
+          <Image
+            source={getImageSource(
+              modalDataList.find((item) => item.title === selectedDisasters.bigButton1)?.imgName
+            )}
+            style={styles.bigButtonImage} // 이미지 스타일 추가
+          />
           <Text style={styles.buttonText}>{selectedDisasters.bigButton1}</Text>
         </TouchableOpacity>
 
@@ -126,11 +145,12 @@ const MainScreen = ({ navigation }) => {
           style={[styles.bigButton, getButtonStyle(selectedDisasters.bigButton2)]}
           onPress={() => handleItemPress(selectedDisasters.bigButton2)}
         >
-          {getIconComponent(
-            modalDataList.find(item => item.title === selectedDisasters.bigButton2)?.iconType || 'Ionicons',
-            modalDataList.find(item => item.title === selectedDisasters.bigButton2)?.icon || 'cloud',
-            65, 'black', styles.buttonIcon
-          )}
+          <Image
+            source={getImageSource(
+              modalDataList.find((item) => item.title === selectedDisasters.bigButton2)?.imgName
+            )}
+            style={styles.bigButtonImage}
+          />
           <Text style={styles.buttonText}>{selectedDisasters.bigButton2}</Text>
         </TouchableOpacity>
       </View>
@@ -140,11 +160,12 @@ const MainScreen = ({ navigation }) => {
           style={[styles.bigButton, getButtonStyle(selectedDisasters.bigButton3)]}
           onPress={() => handleItemPress(selectedDisasters.bigButton3)}
         >
-          {getIconComponent(
-            modalDataList.find(item => item.title === selectedDisasters.bigButton3)?.iconType || 'Ionicons',
-            modalDataList.find(item => item.title === selectedDisasters.bigButton3)?.icon || 'cloud',
-            65, 'black', styles.buttonIcon
-          )}
+          <Image
+            source={getImageSource(
+              modalDataList.find((item) => item.title === selectedDisasters.bigButton3)?.imgName
+            )}
+            style={styles.bigButtonImage}
+          />
           <Text style={styles.buttonText}>{selectedDisasters.bigButton3}</Text>
         </TouchableOpacity>
 
@@ -152,11 +173,12 @@ const MainScreen = ({ navigation }) => {
           style={[styles.bigButton, getButtonStyle(selectedDisasters.bigButton4)]}
           onPress={() => handleItemPress(selectedDisasters.bigButton4)}
         >
-          {getIconComponent(
-            modalDataList.find(item => item.title === selectedDisasters.bigButton4)?.iconType || 'Ionicons',
-            modalDataList.find(item => item.title === selectedDisasters.bigButton4)?.icon || 'cloud',
-            65, 'black', styles.buttonIcon
-          )}
+          <Image
+            source={getImageSource(
+              modalDataList.find((item) => item.title === selectedDisasters.bigButton4)?.imgName
+            )}
+            style={styles.bigButtonImage}
+          />
           <Text style={styles.buttonText}>{selectedDisasters.bigButton4}</Text>
         </TouchableOpacity>
       </View>
@@ -197,7 +219,10 @@ const MainScreen = ({ navigation }) => {
                       style={[styles.iconButton, getButtonStyle(item.title)]}
                       onPress={() => handleDisasterSelection(item.title)}
                     >
-                      {getIconComponent(item.iconType, item.icon)}
+                      <Image
+                        source={getImageSource(item.imgName)}
+                        style={styles.modalImage} // 이미지 스타일 추가
+                      />
                       <Text style={styles.iconText}>{item.title}</Text>
                       {activeDisasters.includes(item.title) && (
                         <Text style={styles.positionIndicator}>{activeDisasters.indexOf(item.title) + 1}</Text>
@@ -217,162 +242,6 @@ const MainScreen = ({ navigation }) => {
   );
 };
 
-const styles = StyleSheet.create({
-  searchContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    borderWidth: 1,
-    borderColor: '#ddd',
-    borderRadius: 5,
-    paddingHorizontal: 10,
-    marginBottom: 10,
-    marginTop: 20,
-  },
-  searchInput: {
-    flex: 1,
-    padding: 10,
-  },
-  searchIcon: {
-    marginLeft: 5,
-  },
-  settingsButton: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    paddingVertical: 5,
-    paddingHorizontal: 10,
-    alignSelf: 'flex-end',
-    borderRadius: 10,
-    marginBottom: 10,
-    borderWidth: 1,
-    borderColor: '#000',
-  },
-  settingsButtonText: {
-    color: '#808080',
-    fontWeight: 'bold',
-    fontSize: 12,
-  },
-  buttonRow: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    marginBottom: 20,
-  },
-  bigButton: {
-    flex: 1,
-    backgroundColor: '#B0E0E6',
-    paddingVertical: 60,
-    marginHorizontal: 10,
-    alignItems: 'center',
-    justifyContent: 'flex-start',
-    borderRadius: 10,
-    position: 'relative',
-    width: 130,
-    height: 130,
-  },
-  buttonIcon: {
-    position: 'absolute',
-    top: 10,
-    left: 10,
-  },
-  buttonText: {
-    color: '#000',
-    fontWeight: 'bold',
-    position: 'absolute',
-    bottom: 10,
-    right: 10,
-    fontSize: 20,
-  },
-  showAllButton: {
-    backgroundColor: '#fff',
-    paddingVertical: 5,
-    paddingHorizontal: 10,
-    alignSelf: 'flex-start',
-    borderRadius: 10,
-    marginBottom: 10,
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.3,
-    shadowRadius: 3,
-    elevation: 3,
-  },
-  showAllButtonText: {
-    color: '#000',
-    fontWeight: 'bold',
-  },
-  listItemButton: {
-    flex: 1,
-    flexDirection: 'row',
-    alignItems: 'center',
-    padding: 15,
-    backgroundColor: '#f9f9f9',
-    borderWidth: 1,
-    borderColor: '#ddd',
-    borderRadius: 10,
-    marginHorizontal: 10,
-    marginVertical: 5,
-  },
-  cellText: {
-    fontSize: 16,
-    textAlign: 'center',
-    flex: 1,
-  },
-  modalContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'rgba(0, 0, 0, 0.5)',
-  },
-  modalContent: {
-    width: '80%',
-    maxHeight: '80%',
-    backgroundColor: '#fff',
-    padding: 20,
-    borderRadius: 10,
-  },
-  modalTitle: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    marginBottom: 20,
-  },
-  iconGrid: {
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    justifyContent: 'space-between',
-  },
-  iconButton: {
-    width: '30%',
-    height: 80,
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginBottom: 20,
-    backgroundColor: '#f0f0f0',
-    borderWidth: 1,
-    borderColor: '#ccc',
-    borderRadius: 10,
-  },
-  iconText: {
-    fontSize: 12,
-    marginTop: 5,
-    textAlign: 'center',
-  },
-  positionIndicator: {
-    position: 'absolute',
-    top: 5,
-    right: 5,
-    fontSize: 10,
-    color: '#000',
-  },
-  closeButton: {
-    marginTop: 20,
-    alignSelf: 'center',
-    backgroundColor: '#ddd',
-    paddingVertical: 10,
-    paddingHorizontal: 20,
-    borderRadius: 5,
-  },
-  closeButtonText: {
-    fontSize: 16,
-    fontWeight: 'bold',
-  },
-});
+
 
 export default MainScreen;
